aa25bb0b9724ba7d09cd9fce14141cf3409d63b3
eapenzacharias <eapenzac@gmail.com>
2021-12-20 22:52:14 +0100

    Table created for missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,35 @@
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Badge,
+  Button,
+  Container,
+  Row,
+  Col,
+  Table,
+} from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { getMissions } from '../redux/missions/missions';
+
+const Mission = (props) => {
+  const {
+    name, desc,
+  } = props;
+
+  return (
+    <tr className="d-flex">
+      <th className="col-2">{name}</th>
+      <td className="col-7">{desc}</td>
+      <td className="col"><Badge bg="secondary">NOT A MEMBER</Badge></td>
+      <td className="col"><Button variant="outline-secondary">Join Mission</Button></td>
+    </tr>
+  );
+};
+
+Mission.propTypes = {
+  name: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+};
 
 const Missions = () => {
   const dispatch = useDispatch();
@@ -11,14 +40,27 @@ const Missions = () => {
   const missions = useSelector((state) => state.missionReducer);
 
   return (
-    <div>
-      This is the Missions page
-      {missions.map((mission) => (
-        <div key={mission.id}>
-          {mission.name}
-        </div>
-      ))}
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Table striped bordered hover vertical-align="middle">
+            <thead>
+              <tr className="d-flex">
+                <th className="col-2">Mission</th>
+                <th className="col-7">Description</th>
+                <th className="col">Status</th>
+                <th className="col"> </th>
+              </tr>
+            </thead>
+            <tbody>
+              {missions.map((mission) => (
+                <Mission key={mission.id} name={mission.name} desc={mission.desc} />
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
