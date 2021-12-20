7d689e60cfb342a00dcd27b4fb06cd9aad3837d4
eapenzacharias <eapenzac@gmail.com>
2021-12-20 22:00:11 +0100

    Mission data printed

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissions } from '../redux/missions/missions';
 
-const Missions = () => (
-  <div>
-    This is the Missions page
-  </div>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, []);
+
+  const missions = useSelector((state) => state.missionReducer);
+
+  return (
+    <div>
+      This is the Missions page
+      {missions.map((mission) => (
+        <div key={mission.id}>
+          {mission.name}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Missions;
